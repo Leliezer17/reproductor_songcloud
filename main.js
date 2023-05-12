bc6cd0107481f3e2d885e9f787b55f32c0671ee3
pol9h7 <joe_miguel_96@hotmaiil.com>
2023-05-12 16:36:14 -0400

    arreglos finales

--- main.js
+++ main.js
@@ -1,18 +1,41 @@
-import { songComponent } from './src/components'
-import { getData } from './src/services'
-import './style.css'
+import './style.css';
+import { getData } from './src/services';
+import { itemSongComponent, currentSongComponent } from './src/components';
+import { addClickListener, removeAllChild, useLocalstorage } from './src/utils';
 
-const container = document.getElementById('container');
+const header = document.getElementById('header')
+const responsiveMenu = document.getElementById('responsive_menu')
+const trackList = document.getElementById('Track_list')
+const currentSong = document.getElementById('current_song')
 
-getData(res => {
+const storage = useLocalstorage('currentSong')
 
-
-  res.map(
-    item => 
-      container.appendChild(
-        songComponent(item)
-      )
-
-  )
-
+getData((res) => {
+  removeAllChild(trackList)
+  res.forEach(element => {
+    trackList.appendChild(
+      itemSongComponent(
+        element, 
+        data => {
+          storage.setItem(data)
+          setCurrentSong(currentSongComponent(data))
+        }
+    ))
+  });
 })
+
+const setCurrentSong = (child) => {
+  removeAllChild(currentSong)
+  currentSong.appendChild(child)
+}
+
+
+addClickListener(responsiveMenu, () => {
+  header.classList.toggle('visible')
+})
+
+
+if (storage.getItem()) {
+  const data = storage.getItem()
+  setCurrentSong(currentSongComponent(data))
+}
---
+++ src/components/index.js
@@ -0,0 +1,30 @@
+export const itemSongComponent = (song, event = ()=>{}) => {
+    const div = document.createElement('div');
+    div.addEventListener('click', () => {
+      event(song)
+    })
+    div.classList.add('song')
+    div.innerHTML = `
+        <img src="${song.path.front}">
+        <div class="description">
+          <h4>${song.title}</h4>
+          <p>${song.author}</p>
+        </div>
+        <p class="duration">${song.duration}</p>
+    `;
+    return div
+  }
+  
+  export const currentSongComponent = (song) => {
+    const div = document.createElement('div');
+    div.classList.add('song')
+    div.innerHTML = `
+        <img src="${song.path.front}">
+        <div class="description">
+          <h4>${song.title}</h4>
+          <p>${song.author}</p>
+        </div>
+        <audio src="${song.path.audio}" controls autoplay>
+    `;
+    return div
+  }
